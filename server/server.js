15ffbb52a850d470ff86147ea9f881f54d8c1a95
Sam Shacter <sshacter@gmail.com>
2024-05-08 10:00:44 -0500

    setting up the server and first send request

--- server/server.js
+++ server/server.js
@@ -16,3 +16,21 @@ app.listen(
     port,
     () => { console.log("Listening on port...", port)}
 )
+
+
+
+let quoteList = [
+  { text: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+  { text: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+  { text: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
+app.get( '/quotes', (req , res) => {
+//write the code for the endpoint/route
+// ! how to send status codes. res.sendStatus(500)
+// res.sendStatus(500)
+//* to send some data
+res.send(quoteList)
+
+
+})
